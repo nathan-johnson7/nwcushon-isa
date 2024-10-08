fce5ef976d0f5282396ed5e137da950793821c9b
njohnson77 <nathanj77777@gmail.com>
2024-10-08 20:51:30 +0100

    Added FindOutMore page

---
+++ src/states/FindOutMore.js
@@ -0,0 +1,13 @@
+//would contain more info and styling
+//placeholder for scenario
+
+const FindOutMore = () => {
+    return (
+        <div>
+            <h2>Find Out More</h2>
+            <p>ISA investments to retail (direct) customers who are not associated with an employer...</p>
+        </div>
+    );
+};
+
+export default FindOutMore;
